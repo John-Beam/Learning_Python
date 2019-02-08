eeaf33e68945f1e399e12931a1c5fb3acc14d228
Ivan <kqj2ic7s7q0n@mail.ru>
2019-02-09 00:06:20 +0300

    004-конструкции управления доступом

--- PycharmProjects/Learning_Python/003_None_Type.py
+++ PycharmProjects/Learning_Python/003_None_Type.py
@@ -2,3 +2,11 @@
 answer = None
 print(type(None))
 #<class 'NoneType'>
+print(bool(None))
+#преборазование к тииу bool всегда даёт False
+#Можно попробовать и так ( всё равно будет преобразование)
+answer = None
+if not answer:
+    print("ответ не получен")
+    #на экране будет "ответ не получен"
+
